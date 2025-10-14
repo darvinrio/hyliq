d50ce1dde309b405beccdebe7f3dc431c1ae844a
darvin <todarvinrio@gmail.com>
2025-10-15 01:39:07 +0530

    migrate dnhype portfolio to workflow, actions init

--- loaders/actions.py
+++ loaders/actions.py
@@ -1,11 +1,13 @@
 import polars as pl
 import os
 from config import cache_dir
+from cchecksum import to_checksum_address
 
 
 def get_actions(address: str, use_cache: bool = True) -> pl.DataFrame:
     actions_dir = os.path.join("src")
     os.makedirs(actions_dir, exist_ok=True)
-    actions_file = os.path.join(actions_dir, f"{address.lower()}.csv")
+    file_name = f"{to_checksum_address(address)}"
+    actions_file = os.path.join(actions_dir, f"{file_name}.csv")
     df_actions = pl.read_csv(actions_file, infer_schema_length=0)
     return df_actions
--- main.py
+++ main.py
@@ -1,39 +1,11 @@
 from loguru import logger
-from loaders.portfolio import get_portfolio, combine_portfolios
 import polars as pl
 
-from viz.portfolio import visualize_portfolio
+from loaders.actions import get_actions
 
 dnhype_short_eoa = "0x1Da7920cA7f9ee28D481BC439dccfED09F52a237"
 dnhype_spot_eoa = "0xca36897cd0783a558f46407cd663d0f46d2f3386"
 
-dnhype_short_portfolio = get_portfolio(address=dnhype_short_eoa).filter(
-    pl.col("period") == "allTime"
-)
-dnhype_spot_portfolio = get_portfolio(address=dnhype_spot_eoa).filter(
-    pl.col("period") == "allTime"
-)
+d = get_actions(dnhype_short_eoa)
 
-# Combine the two portfolios
-combined_portfolio = combine_portfolios([dnhype_short_portfolio, dnhype_spot_portfolio])
-
-# logger.debug(f"Individual Short Portfolio:\n{dnhype_short_portfolio.tail()}")
-# logger.debug(f"Individual Spot Portfolio:\n{dnhype_spot_portfolio.tail()}")
-# logger.debug(f"Combined Portfolio:\n{combined_portfolio.tail()}")
-
-short_viz = visualize_portfolio(
-    dnhype_short_portfolio, title=f"dnHYPE Short Portfolio - {dnhype_short_eoa}"
-)
-spot_viz = visualize_portfolio(
-    dnhype_spot_portfolio, title=f"dnHYPE Spot Portfolio - {dnhype_spot_eoa}"
-)
-combined_viz = visualize_portfolio(
-    combined_portfolio, title="Combined dnHYPE Strategy Portfolio"
-)
-
-viz = (
-    (spot_viz & short_viz & combined_viz)
-    # .resolve_scale(y="independent")
-    .properties(title="DNHYPE Portfolio Overview")
-)
-viz.save("portfolio.html")
+logger.debug(d.tail())
